0a2887c75a6db2ec2ebe39552e1de36d8bdbeffa
Crowdbotics <team@crowdbotics.com>
2020-06-19 18:55:48 +0000

    Update models

--- backend/componentlayout1_dev_6277/wsgi.py
+++ backend/componentlayout1_dev_6277/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'componentlayout1_dev_6277.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "componentlayout1_dev_6277.settings")
 
 application = get_wsgi_application()
